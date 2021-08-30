f944ad67fbb420caef7a3fdc99bf96b95d38a870
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-08-30 17:31:32 +0700

    feat: add NavBar

--- src/components/Products/Product/index.jsx
+++ src/components/Products/Product/index.jsx
@@ -9,7 +9,7 @@ const Product = ({ product }) => {
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image='' title={product.name} />
+      <CardMedia className={classes.media} image={product.image} title={product.name} />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant='h5' gutterBottom>
@@ -17,11 +17,11 @@ const Product = ({ product }) => {
           </Typography>
           <Typography variant='h5'>{product.price}</Typography>
         </div>
-        <Typography variant='h2' color='textSecondary'>
+        <Typography variant='body2' color='textSecondary'>
           {product.desc}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing className={classes.CardActions}>
+      <CardActions disableSpacing className={classes.cardActions}>
         <IconButton aria-label='Add to Cart'>
           <AddShoppingCart />
         </IconButton>
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product';
 
+import useStyles from './styles';
+
 const products = [
-  { id: 1, name: 'ASUS', desc: 'Laptop asus', price: '$5' },
-  { id: 2, name: 'car', desc: 'Sport Car', price: '$20' },
+  { id: 1, name: 'ASUS', desc: 'Laptop asus', price: '$5', image: 'https://placeimg.com/640/480/tech' },
+  { id: 2, name: 'Lenovo', desc: 'Flagship', price: '$20', image: 'https://placeimg.com/640/480/tech' },
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container spacing={4} justify='center'>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
